e2d93963a97d822b63e1666c37900d03bab22b74
Ayush01010101 <ayushawachar0989@gmail.com>
2025-09-15 17:08:34 +0530

    Project intial-stage done

--- src/Components/CreatePost.tsx
+++ src/Components/CreatePost.tsx
@@ -1,22 +1,36 @@
 import type { FormEvent, ReactNode } from "react"
 import { useState } from "react"
 import { useMutation } from "@tanstack/react-query"
+import SupabaseClient from "../Instances/SupabaseClient"
 const Createpost = (): ReactNode => {
-  const [title, setTitle] = useState<string>('')
-  const [content, setContent] = useState<string>('')
-
-
-  const HandleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  interface PostType {
+    title: string;
+    content: string;
 
   }
+  const [title, setTitle] = useState<string>('')
+  const [content, setContent] = useState<string>('')
+  const createpostfunx = async (post: PostType) => {
+    const { data, error } = await SupabaseClient.from("Posts").insert(post)
+
+    if (error) {
+      throw new Error("user not authenticate to post")
+    }
+    return data;
+  }
+  const { mutate
+  } = useMutation({ mutationFn: createpostfunx })
+  const HandleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    mutate({ title, content })
+  }
   return (
-    < div className="p-2" >
+    < div className="p-2 text-white" >
 
-      <form onSubmit={(e) => { HandleSubmit(e) }}>
+      <form className="flex flex-col w-fit " onSubmit={(e) => { HandleSubmit(e) }}>
 
 
-        <input className="" type="text" onChange={(e) => setTitle(e.target.value)} value={title} />
+        <input className="text-gray-300 " placeholder="Enter Title" type="text" onChange={(e) => setTitle(e.target.value)} value={title} />
         <textarea placeholder="enter your contet" className="p-3" onChange={(e) => setContent(e.target.value)} value={content} />
         <button type="submit">create post</button>
       </form>
--- src/Pages/CreatePostPage.tsx
+++ src/Pages/CreatePostPage.tsx
@@ -2,10 +2,11 @@ import type { ReactNode } from "react";
 import Createpost from "../Components/CreatePost";
 const CreatepostPage = (): ReactNode => {
   return (
-    <div className="h-screen w-screen bg-[rgba(10,10,10,1)]">
-
+    <div className="h-screen  pt-24 w-screen bg-[rgba(10,10,10,1)]">
       <Createpost />
+
     </div>
+
   )
 
 }
