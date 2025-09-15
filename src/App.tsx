ff9727c40b7721e059e565a7a211f0cbe17c411b
Ayush01010101 <ayushawachar0989@gmail.com>
2025-09-15 16:35:16 +0530

    auth context added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-
+import { Route, Routes } from "react-router"
+import Hompage from "./Pages/Hompage"
+import Navbar from "./Components/Navbar"
+import CreatepostPage from "./Pages/CreatePostPage"
 function App() {
-
   return (
     <>
+      <Navbar />
+      <Routes>
+
+
+        <Route path="/" element={<Hompage />} />
+        <Route path="/createpost" element={<CreatepostPage />} />
+      </Routes>
 
     </>
   )
---
+++ src/Pages/CreatePostPage.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+import Createpost from "../Components/CreatePost";
+const CreatepostPage = (): ReactNode => {
+  return (
+    <div className="h-screen w-screen bg-[rgba(10,10,10,1)]">
+
+      <Createpost />
+    </div>
+  )
+
+}
+
+
+
+export default CreatepostPage
