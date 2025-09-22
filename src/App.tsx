c1dbb97998cf88736254ce6d6c7d76433b3aa782
Ayush01010101 <ayushawachar0989@gmail.com>
2025-09-22 15:31:24 +0530

    like and dislike structure ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { Route, Routes } from "react-router"
-import Hompage from "./Pages/Hompage"
-import PostDetailPage from "./Pages/PostDetailPage"
-import Navbar from "./Components/Navbar"
-import CreatepostPage from "./Pages/CreatePostPage"
+import { Route, Routes } from "react-router";
+import Hompage from "./Pages/Hompage";
+import PostDetailPage from "./Pages/PostDetailPage";
+import Navbar from "./Components/Navbar";
+import CreatepostPage from "./Pages/CreatePostPage";
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Hompage />} />
+
         <Route path="/post/:id" element={<PostDetailPage />} />
         <Route path="/createpost" element={<CreatepostPage />} />
-      </Routes >
-
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
