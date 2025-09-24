91281e2e11ae951117b874b2b4d7911e8d949417
Ayush01010101 <ayushawachar0989@gmail.com>
2025-09-25 03:01:32 +0530

    create community page structure ready

---
+++ src/Components/CreateCommunity.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react";
+
+const CreateCommunity: React.FC = (): ReactNode => {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default CreateCommunity
---
+++ src/Pages/CreateCommunityPage.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode, FC } from "react";
+import { useParams } from "react-router";
+import CreateCommunity from "../Components/CreateCommunity";
+
+const CreateCommunityPage: FC = (): ReactNode => {
+  return (
+    <div>
+
+      <CreateCommunity } />
+    </div>
+  )
+}
+
+export default CreateCommunityPage
