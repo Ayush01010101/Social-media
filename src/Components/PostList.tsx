ef2395d9bf0f25f12b7d7e022fbce843e6ada542
Ayush01010101 <ayushawachar0989@gmail.com>
2025-09-26 19:30:51 +0530

    removed fetch request from windowactive and component mount

--- src/Components/PostList.tsx
+++ src/Components/PostList.tsx
@@ -17,14 +17,12 @@ export interface PostType {
 const PostList: FC = () => {
 
 
-  const { User } = useAuth()
   const FetchPost = async (): Promise<PostType[]> => {
     const { data, error } = await SupabaseClient.from('Posts').select('*').order('created_at', { ascending: false })
     if (error) throw new Error("failed to fetch posts")
     return data as PostType[]
   }
-  const { data, error } = useQuery({ queryKey: ["postlist"], queryFn: FetchPost })
-  console.log(data)
+  const { data, error } = useQuery({ refetchOnMount: false, refetchOnWindowFocus: false, queryKey: ["postlist"], queryFn: FetchPost })
   return (
     <div className="flex sm:mx-0 mx-2 flex-col  items-center gap-6 ">
       {data?.map((post) => (
