4a064f09b3ed0f74cc71cf2b3734045d8ae2433b
Ayush01010101 <ayushawachar0989@gmail.com>
2025-09-29 17:06:32 +0530

    like functionality

---
+++ src/Components/Like.tsx
@@ -0,0 +1,59 @@
+import { useState, type FC, type ReactNode } from "react";
+import { Heart } from 'lucide-react'
+import SupabaseClient from "../Instances/SupabaseClient";
+import { useAuth } from "../Context/AuthContext";
+import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query";
+import type { Likes } from "./PostList";
+interface props {
+  postID: number;
+  LikesARR: Likes[]
+  like: number
+}
+
+
+
+const Like: FC<props> = ({ postID, like, LikesARR }): ReactNode => {
+  const queryclient = useQueryClient()
+  const { User } = useAuth()
+  const isLiked = LikesARR.find((obj: Likes) => obj.post_id == postID && obj.user_id == User?.id)
+  async function getLike() {
+    const { data, error } = await SupabaseClient
+      .from('Likes')
+      .select('*')
+      .eq('user_id', User?.id)
+      .eq('post_id', postID);
+
+    return data;
+  }
+
+  async function addLike() {
+    if (isLiked) {
+      const { data, error } = await SupabaseClient.from("Likes").delete().match({
+        user_id: User?.id,
+        post_id: postID
+      })
+    } else {
+      const { data, error } = await SupabaseClient.from("Likes").insert({
+        user_id: User?.id,
+        post_id: postID,
+        likes: 1
+      })
+
+    }
+  }
+
+
+  const { mutate, error } = useMutation({
+    mutationFn: addLike, onSuccess: () => {
+      queryclient.invalidateQueries({ queryKey: ['postlist'] })
+    }
+  })
+  return (
+    <div onClick={() => mutate()} className="flex items-center text-gray-400 hover:text-gray-200 flex-wrap gap-2 ">
+      <Heart fill={isLiked ? 'red' : ''} size={18} />
+      <div>{like}</div>
+
+    </div>
+  )
+}
+export default Like;
