f89b87308faaa7c4b2e441ea98d3ccf01a3a665e
Ayush01010101 <ayushawachar0989@gmail.com>
2025-10-07 17:48:51 +0530

    comment replies style added

--- src/Components/CommentList.tsx
+++ src/Components/CommentList.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import SupabaseClient from "../Instances/SupabaseClient";
 import RepliesList from "./RepliesList";
 import { formatDistanceToNow } from "date-fns";
+import { Heart } from "lucide-react";
 
 const CommentList = ({ postid }: { postid: number }): ReactNode => {
   console.log(postid)
@@ -37,9 +38,12 @@ const CommentList = ({ postid }: { postid: number }): ReactNode => {
             return (
               <div className="p-6   shadow-[1px_2px_5px_1px_rgba(0,0,0,0.7)] shadow-black/45  text-gray-300 flex flex-col  rounded-md">
                 <div className="text-sm py-1 flex gap-3  items-center">
+                  <img src="https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg" className="h-6 rounded-2xl" />
                   <p>{comment.author_name}</p>
+
                   <p className="p-2 bg-black/20 rounded-xl"> {formatDistanceToNow(new Date(comment.created_at), { addSuffix: true })}</p>
 
+                  <Heart size={20} />
                 </div>
                 <div className="font-medium text-xl">
 
--- src/Components/RepliesCard.tsx
+++ src/Components/RepliesCard.tsx
@@ -4,6 +4,7 @@ import type { FC } from "react";
 import { useQuery } from "@tanstack/react-query"
 import SupabaseClient from "../Instances/SupabaseClient";
 import { useAuth } from "../Context/AuthContext";
+import { ThumbsUp } from "lucide-react";
 
 export interface RepliesType {
   id: number
@@ -15,7 +16,7 @@ export interface RepliesType {
 }
 
 async function FetchReplies(commentid: number, userid: string): Promise<RepliesType[] | null> {
-  const { data, error } = await SupabaseClient.from("Replies").select("*").eq('comment_id', commentid)
+  const { data, error } = await SupabaseClient.from("Replies").select("*").eq('comment_id', commentid).order('created_at', { ascending: false })
   console.log(data)
   if (error) return null;
   return data as RepliesType[]
@@ -26,16 +27,19 @@ const RepliesCard: FC<{ commentid: number }> = ({ commentid }): ReactNode => {
   if (!User?.id) return
   const { data } = useQuery({ queryFn: () => FetchReplies(commentid, User.id), queryKey: [`Replies-${commentid}`], refetchOnWindowFocus: false, refetchOnMount: false })
   return (
-    <div className="bg-[#1e1e1e] flex gap-5 flex-col rounded-xl p-3 mt-5 max-h-1/2 ">
-      <div className="flex max-h-96  no-scrollbar  flex-col gap-5 overflow-auto">
+    <div className="bg-[#121212]  flex gap-5 flex-col rounded-xl  mt-5 max-h-1/2 ">
+      <div className="flex max-h-96 no-scrollbar gap-5  flex-col  overflow-auto">
 
         {data?.map((Reply: RepliesType): ReactNode => {
 
           return (
-            <div key={Reply.id} className="ml-4 shadow-[1px_2px_5px_1px_rgba(0,0,0,0.7)] shadow-black/45  text-white flex flex-col   rounded-md">
+            <div key={Reply.id} className="ml-4 py-5 px-4  shadow-[1px_2px_5px_1px_rgba(0,0,0,0.7)] shadow-black/45 text-white flex flex-col   rounded-md">
               <div className="text-sm py-1 flex gap-3  items-center">
+                <img src="https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg" className="h-6 rounded-2xl" />
+
                 <p>{Reply.author_name}</p>
                 <p className="p-2 bg-black/20 rounded-xl"> {formatDistanceToNow(new Date(Reply.created_at), { addSuffix: true })}</p>
+                <ThumbsUp className="opacity-90" color="white" />
 
               </div>
               <div className="font-medium text-xl">
